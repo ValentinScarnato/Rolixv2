cee188bc2c647c51be2773d6e408d756c25d8dc5
ValentinScarnato <155959182+ValentinScarnato@users.noreply.github.com>
2026-01-29 09:42:12 +0100

    service updated

--- Rolix.Web/Services/DataverseService.cs
+++ Rolix.Web/Services/DataverseService.cs
@@ -14,11 +14,11 @@ public class DataverseService
 
     public ServiceClient GetClient()
     {
-        // Si le client est déjà connecté et prêt, on le retourne directement
+        // Si le client est déjà connecté, on le retourne directement
         if (_client != null && _client.IsReady)
             return _client;
 
-        // Récupération des infos depuis secrets.json (ou appsettings.json)
+        // Récupération des infos depuis secrets.json (fichier config)
         var url = _config["Dataverse:Url"];
         var appId = _config["Dataverse:AppId"];
         var secret = _config["Dataverse:ClientSecret"];
@@ -27,18 +27,17 @@ public class DataverseService
 
         string connectionString;
 
-        // SCÉNARIO 1 : Tu as retrouvé le Secret (Idéal)
+        
         if (!string.IsNullOrEmpty(secret))
         {
             connectionString = $"AuthType=ClientSecret;Url={url};ClientId={appId};ClientSecret={secret};";
         }
-        // SCÉNARIO 2 : Tu utilises Login/Mot de passe (Ton cas actuel)
+        
         else
         {
-            // ID générique de Microsoft
             var finalAppId = !string.IsNullOrEmpty(appId) ? appId : "51f81489-12ee-4a9e-aaae-a2591f45987d";
 
-            // MODIFICATION ICI : On passe en mode "Interactif" pour gérer la 2FA
+            // A2F
             connectionString = $@"
                 AuthType=OAuth;
                 Url={url};
@@ -47,10 +46,6 @@ public class DataverseService
                 RedirectUri=http://localhost;
                 LoginPrompt=Auto;
                 RequireNewInstance=True";
-
-            // NOTE : J'ai retiré "Password={pass};" volontairement.
-            // Une fenêtre Microsoft va s'ouvrir au lancement du site pour te demander ton mot de passe 
-            // et te permettre de valider la notif sur ton téléphone.
         }
 
         try
